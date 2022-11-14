daa686efba088a33d7fe943cc29224b5df1e2e7b
Rort <thetrueroadman@gmail.com>
2022-11-14 17:50:58 -0500

    Dice output result dependent on input

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function roll() {
-  return Math.floor(Math.random() * 6) + 1;
+function roll(sides) {
+  return Math.floor(Math.random() * sides) + 1;
 }
-
 let rollbtn = document.getElementById("rollbtn");
 rollbtn.addEventListener("click", e => {
-  let result = roll();
-  document.getElementById("output").innerHTML = `You rolled a ${result}`;
+  let number = document.getElementById("diceAmount").value;
+  let sides = document.getElementById("diceSides").value;
+  let out = "";
+  let total = 0;
+
+  for (let i = 0; i < number; i++) {
+    let result = roll(sides);
+    out += `<p>You rolled a ${result}</p>`;
+    total += result;
+  }
+
+  let totalOut = document.createElement("p");
+  totalOut.appendChild(document.createTextNode(`Total: ${total}`));
+
+  document.getElementById("output").innerHTML = out;
+  document.getElementById("output").appendChild(totalOut);
 })
