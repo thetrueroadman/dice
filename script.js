d2a767f6726ae670131696cd00bae3f1a33230b0
Rort <thetrueroadman@gmail.com>
2022-11-15 18:58:10 -0500

    Added Roboto font

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ rollbtn.addEventListener("click", e => {
     }
   }
 
-  let average = (total / number);
+  let average = document.createElement("p");
+  average.appendChild(document.createTextNode(`Average: ${total / number}`));
 
   let totalOut = document.createElement("p");
   totalOut.appendChild(document.createTextNode(`Total: ${total}`));
 
   document.getElementById("output").innerHTML = out;
   document.getElementById("output").appendChild(totalOut);
+  document.getElementById("output").appendChild(average);
 })
